e28f98fbeac70ff5ee1f79eacf89790efae8da78
ilshatG <mailto:ilshatrashidovich@rambler.ru>
2018-09-08 07:23:46 +0500

    1.3. JavDoc. Code convetion.

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,6 +1,18 @@
 package ru.job4j.calculate;
 
+/**
+* Calculate
+* Данный класс предназначен для запуска программы
+* @author Ilshat Gaifutdinov (ilshatrashidovich@rambler.ru)
+* @since 08/09/2018
+* @version 0.1
+*/
 public class Calculate {
+	
+	/**
+	* С данного метода начинается выполнение программы.
+	* Выводит на экран текст приветствия
+	*/
 	public static void main(String[] args) {
 		System.out.println("Hello new brave world!");
 	}
