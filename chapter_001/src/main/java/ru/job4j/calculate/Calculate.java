655cece4710fa0f511ef7db81b7c401a9d1a7cb5
ilshatG <mailto:ilshatrashidovich@rambler.ru>
2018-09-08 09:59:31 +0500

    1.5. Checkstyle [#4123]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,8 +1,8 @@
 package ru.job4j.calculate;
 
 /**
-* Calculate
-* Данный класс предназначен для запуска программы
+* Calculate.
+* Данный класс предназначен для запуска программы.
 * @author Ilshat Gaifutdinov (ilshatrashidovich@rambler.ru)
 * @since 07/09/2018
 * @version 0.1
@@ -10,10 +10,21 @@ package ru.job4j.calculate;
 public class Calculate {
 	
 	/**
+	* main
 	* С данного метода начинается выполнение программы.
 	* Выводит на экран текст приветствия
+	* @param args - args
 	*/
 	public static void main(String[] args) {
 		System.out.println("Hello new brave world!");
 	}
+
+	/**
+	* Method echo.
+	* @param name Your name.
+	* @return Echo, echo, echo :  plus your name.
+	*/
+	public String echo(String name) {
+		return "Echo, echo, echo : " + name;
+	}	
 }
