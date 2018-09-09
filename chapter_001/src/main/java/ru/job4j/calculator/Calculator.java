c84cd85cdf83c8172699cd9b23617cf19f9512fb
ilshatG <mailto:ilshatrashidovich@rambler.ru>
2018-09-09 12:22:38 +0500

    3.1. Элементарный калькулятор. [#185]

---
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -0,0 +1,66 @@
+package ru.job4j.calculator;
+
+/**
+ * Calculator.
+ * Объект данного классна создается для выполнения арифметических операций.
+ * @author Ilshat Gaifutdinov
+ * @version 0.1
+ * @since 0.1
+ */
+public class Calculator {
+    private double result;
+
+    /**
+     * Арифметически складывает два значения.
+     * @param first первый параметр.
+     * @param second второй параметр.
+     * @author Ilshat Gaifutdinov
+     * @since 0.1
+     */
+    public void add(double first, double second) {
+        this.result = first + second;
+    }
+
+    /**
+     * Арифметически вычитает из первого значения второе.
+     * @param first первый параметр.
+     * @param second второй параметр.
+     * @author Ilshat Gaifutdinov
+     * @since 0.1
+     */
+    public void subtract(double first, double second) {
+        this.result = first - second;
+    }
+
+
+    /**
+     * Производит арифметическую оперецию деления.
+     * @param first первый параметр.
+     * @param second второй параметр.
+     * @author Ilshat Gaifutdinov
+     * @since 0.1
+     */
+    public void div(double first, double second) {
+        this.result = first / second;
+    }
+
+    /**
+     * Производит арифметическую оперецию умножения.
+     * @param first первый параметр.
+     * @param second второй параметр.
+     * @author Ilshat Gaifutdinov
+     * @since 0.1
+     */
+    public void multiple(double first, double second) {
+        this.result = first * second;
+    }
+
+    /**
+     * @return результат послендней выполненной арифметической операции посредством вызова
+     * одного из соответствующих методов данного объекта
+     */
+    public double getResult() {
+        return this.result;
+    }
+
+}
---
+++ chapter_001/src/test/java/ru/job4j/calculator/CalculatorTest.java
@@ -0,0 +1,52 @@
+package ru.job4j.calculator;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Для тестирования методов в классе Calculate.
+ * @author Ilshat Gaifutdinov
+ * @version 0.1
+ * @since 0.1
+ */
+public class CalculatorTest {
+    @Test
+    public void whenAddOnePlusOneThenTwo() {
+        Calculator calc = new Calculator();
+        calc.add(1D, 1D);
+        double result = calc.getResult();
+        double expected = 2D;
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenSubtractThreeMinusOneThenTwo() {
+        Calculator calc = new Calculator();
+        calc.subtract(3D, 1D);
+        double result = calc.getResult();
+        double expected = 2D;
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenMultipleTwoAndTwoThenFour() {
+        Calculator calc = new Calculator();
+        calc.multiple(2D, 2D);
+        double result = calc.getResult();
+        double expected = 4D;
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenDivideSixToThreeThenTwo() {
+        Calculator calc = new Calculator();
+        calc.div(6D, 3D);
+        double result = calc.getResult();
+        double expected = 2D;
+        assertThat(result, is(expected));
+    }
+
+
+}
