bc975d92b864dfccc8d7f19ebabf19b59fcbde89
ilshatG <mailto:ilshatrashidovich@rambler.ru>
2018-09-11 14:25:31 +0500

    исправление метода max()

--- chapter_001/src/main/java/ru/job4j/max/Max.java
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -25,7 +25,6 @@ public class Max {
      * @return Максимумальное из трех заданных значений.
      */
     public int max(int first, int second, int third) {
-        int temp = max(first, second);
-        return max(temp, third);
+        return max( max(first, second), third);
     }
 }
