bbe93778c465631fd55f06b0c1fea38aeb03ebdb
ilshatG <mailto:ilshatrashidovich@rambler.ru>
2018-09-11 14:22:08 +0500

    Тесты для метода max()

--- chapter_001/src/test/java/ru/job4j/max/MaxTest.java
+++ chapter_001/src/test/java/ru/job4j/max/MaxTest.java
@@ -20,10 +20,39 @@ public class MaxTest {
     }
 
     @Test
+    public void whenFirstGreaerSecond() {
+        Max maxim = new Max();
+        int result = maxim.max(6, 2);
+        assertThat(result, is(6));
+    }
+
+    @Test
+    public void whenFirstEqualsSecond() {
+        Max maxim = new Max();
+        int result = maxim.max(2, 2);
+        assertThat(result, is(2));
+    }
+
+
+    @Test
     public void whenThirdGraterThanFirstAndSecond() {
         Max maxim = new Max();
         int result = maxim.max(1, 3, 4);
         assertThat(result, is(4));
+    }
+
+    @Test
+    public void whenFirstGraterThanThirdAndSecond() {
+        Max maxim = new Max();
+        int result = maxim.max(7, 3, 4);
+        assertThat(result, is(7));
+    }
+
+    @Test
+    public void whenFirstEqualsThirdAndSecond() {
+        Max maxim = new Max();
+        int result = maxim.max(8, 8, 8);
+        assertThat(result, is(8));
     }
 
 }
