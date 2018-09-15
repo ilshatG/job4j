339cf30f6d4620d5676468072d2e65cb75655b65
ilshatG <mailto:ilshatrashidovich@rambler.ru>
2018-09-15 18:26:58 +0500

    ArrayDuplicate

--- chapter_001/src/main/java/ru/job4j/array/ArrayDuplicate.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayDuplicate.java
@@ -8,14 +8,11 @@ public class ArrayDuplicate {
         for (int i = 0; i < array.length; i++) {
             for (int j = i + 1; j < array.length - duplicates; j++) {
                 if (array[i].equals(array[j])) {
-                    while (j != array.length - duplicates - 1
-                            && array[array.length - duplicates - 1].equals(array[j])) {
-                        duplicates++;
-                    }
                     String tmp = array[array.length - duplicates - 1];
                     array[array.length - duplicates - 1] = array[j];
                     array[j] = tmp;
                     duplicates++;
+                    j--;
                 }
             }
         }
