54e204819b8d8f39a952429f3a69c44075d4560c
Ilshat Gaifutdinov <ilshatrashidovich@rambler.ru>
2018-09-25 00:05:24 +0500

    menuTracker

---
+++ chapter_002/src/main/java/ru/job4j/tracker/MenuTracker.java
@@ -0,0 +1,103 @@
+package ru.job4j.tracker;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MenuTracker {
+    /**
+     * @param хранит ссылку на объект .
+     */
+    private Input input;
+    /**
+     * @param хранит ссылку на объект .
+     */
+    private Tracker tracker;
+    /**
+     * @param хранит ссылку на массив типа UserAction.
+     */
+    private List<UserAction> actions = new ArrayList<>();
+
+    /**
+     * Конструктор.
+     *
+     * @param input   объект типа Input
+     * @param tracker объект типа Tracker
+     */
+    public MenuTracker(Input input, Tracker tracker) {
+        this.input = input;
+        this.tracker = tracker;
+    }
+
+    /**
+     * Метод для получения массива меню.
+     *
+     * @return длину массива
+     */
+    public int getActionsLentgh() {
+        return this.actions.size();
+    }
+
+    /**
+     * Метод заполняет массив.
+     */
+    public void fillActions() {
+        this.actions.add(new AddItem(0, "Add program"));
+        this.actions.add(new ShowItems(1, "Show all items"));
+        this.actions.add(new MenuTracker.EditItem(2, "Edit item"));
+        this.actions.add(new MenuTracker.DeleteItem(3, "Delete item"));
+        this.actions.add(new FindItemById(4, "Find item by Id"));
+        this.actions.add(new FindItemsByName(5, "Find items by name"));
+        this.actions.add(new ExitProgram(6, "Exit Program"));
+    }
+
+    /**
+     * Метод в зависимости от указанного ключа, выполняет соотвествующие действие.
+     *
+     * @param key ключ операции
+     */
+    public void select(int key) {
+        this.actions.get(key).execute(this.input, this.tracker);
+    }
+
+    /**
+     * Метод выводит на экран меню.
+     */
+    public void show() {
+        for (UserAction action : this.actions) {
+            if (action != null) {
+                System.out.println(action.info());
+            }
+        }
+    }
+
+    class AddItem implements UserAction {
+        private int key;
+        private String info;
+
+        AddItem(int key, String info){
+            this.key = key;
+            this.info = info;
+        }
+
+        @Override
+        public int key() {
+            return this.key;
+        }
+
+        @Override
+        public String info() {
+            return this.info;
+        }
+
+        @Override
+        public void execute(Input input, Tracker tracker) {
+            System.out.println("------------ Добавление новой заявки --------------");
+            String name = input.ask("Введите имя заявки :");
+            String desc = input.ask("Введите описание заявки :");
+            Item item = new Item(name, desc);
+            tracker.add(item);
+            System.out.println("------------ Новая заявка с getId : " + item.getId() + "-----------");
+
+        }
+    }
+}
