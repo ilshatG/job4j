5ebe25773b20a2187a0585e0bac8fb42d69095f8
ilshatG <mailto:ilshatrashidovich@rambler.ru>
2018-09-26 21:50:55 +0500

    3. if-else-throw [#49895]

--- chapter_002/src/main/java/ru/job4j/tracker/ConsoleInput.java
+++ chapter_002/src/main/java/ru/job4j/tracker/ConsoleInput.java
@@ -19,10 +19,9 @@ public class ConsoleInput implements Input {
                 break;
             }
         }
-        if (exist) {
-            return key;
-        } else {
+        if (!exist) {
             throw new MenuOutException("Неверное значение");
         }
+        return key;
     }
 }
