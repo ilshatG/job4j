e3f1069076005cf912c2d6ffe261bf84e32dc978
ilshatG <mailto:ilshatrashidovich@rambler.ru>
2018-10-17 09:15:06 +0500

    Крестики-нолики на JavaFX [#49777]

--- chapter_001/src/main/java/ru/job4j/tictactoe/Logic3T.java
+++ chapter_001/src/main/java/ru/job4j/tictactoe/Logic3T.java
@@ -10,27 +10,17 @@ public class Logic3T {
     }
 
     public boolean isWinnerX() {
-        return this.filledBy(true, 0, 0, 1, 0)
-                || this.filledBy(true, 0, 0, 0, 1)
-                || this.filledBy(true, 0, 0, 1, 1)
-                || this.filledBy(true, table.length - 1, 0, -1, 1)
-                || this.filledBy(true, 0, 1, 1, 0)
-                || this.filledBy(true, 0, table.length - 1, 1, 0)
-                || this.filledBy(true, 1, 0, 0, 1)
-                || this.filledBy(true, table.length - 1, 0, 0, 1);
+        return this.fillBy(Figure3T::hasMarkX, 0, 0, 1, 0)
+                || this.fillBy(Figure3T::hasMarkX, 0, 0, 0, 1)
+                || this.fillBy(Figure3T::hasMarkX, 0, 0, 1, 1)
+                || this.fillBy(Figure3T::hasMarkX, 2, 0, -1, 1)
+                || this.fillBy(Figure3T::hasMarkX, 0, 1, 1, 0)
+                || this.fillBy(Figure3T::hasMarkX, 0, 2, 1, 0)
+                || this.fillBy(Figure3T::hasMarkX, 1, 0, 0, 1)
+                || this.fillBy(Figure3T::hasMarkX, 2, 0, 0, 1);
     }
 
-    public boolean isWinnerO() {
-        return this.filledBy(false, 0, 0, 1, 0)
-                || this.filledBy(false, 0, 0, 0, 1)
-                || this.filledBy(false, 0, 0, 1, 1)
-                || this.filledBy(false, table.length - 1, 0, -1, 1)
-                || this.filledBy(false, 0, 1, 1, 0)
-                || this.filledBy(false, 0, table.length - 1, 1, 0)
-                || this.filledBy(false, 1, 0, 0, 1)
-                || this.filledBy(false, table.length - 1, 0, 0, 1);
-    }
-/* с предикатами. оставлено на будущее
+
     public boolean isWinnerO() {
         return this.fillBy(Figure3T::hasMarkO, 0, 0, 1, 0)
                 || this.fillBy(Figure3T::hasMarkO, 0, 0, 0, 1)
@@ -41,7 +31,7 @@ public class Logic3T {
                 || this.fillBy(Figure3T::hasMarkO, 1, 0, 0, 1)
                 || this.fillBy(Figure3T::hasMarkO, 2, 0, 0, 1);
     }
-*/
+
     public boolean hasGap() {
         boolean result = false;
         for (Figure3T[] figure : table) {
@@ -58,30 +48,7 @@ public class Logic3T {
         return result;
     }
 
-    //boolean xOrOchecking для проверки X, если true, иначе O проверяем
-    public boolean filledBy(boolean xOrOchecking, int startX, int startY, int deltaX, int deltaY) {
-        boolean result = true;
-        for (int index = 0; index != this.table.length; index++) {
-            Figure3T cell = this.table[startY][startX];
-            if (xOrOchecking) {
-                if (!cell.hasMarkX()) {
-                    result = false;
-                    break;
-                }
-            } else {
-                if (!cell.hasMarkO()) {
-                    result = false;
-                    break;
-                }
-            }
-            startX += deltaX;
-            startY += deltaY;
-        }
-        return result;
-    }
 
-
-    /* С предикатом. Оставлено на будущее
     public boolean fillBy(Predicate<Figure3T> predicate, int startX, int startY, int deltaX, int deltaY) {
         boolean result = true;
         for (int index = 0; index != this.table.length; index++) {
@@ -94,5 +61,5 @@ public class Logic3T {
             }
         }
         return result;
-    }*/
+    }
 }
