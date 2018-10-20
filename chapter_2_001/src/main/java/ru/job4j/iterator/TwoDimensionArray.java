b766eccff4ed7b35552a372642badf1323f23d6d
ilshatG <mailto:ilshatrashidovich@rambler.ru>
2018-10-20 20:48:27 +0500

    5.1.1. Итератор для двухмерного массива int[][] #76758 2

--- chapter_2_001/src/main/java/ru/job4j/iterator/TwoDimensionArray.java
+++ chapter_2_001/src/main/java/ru/job4j/iterator/TwoDimensionArray.java
@@ -7,51 +7,32 @@ import java.util.function.Consumer;
 
 public class TwoDimensionArray implements Iterator {
     private int[][] values;
-    private int index = 0;
+    private int row = 0;
+    private int columnIndex = 0;
 
     public TwoDimensionArray(int[][] values) {
         this.values = values;
     }
 
-    private int arrayLength() {
-        int length = 0;
-        for (int[] array : values) {
-            length += array.length;
-        }
-        return length;
-    }
-
-    int getItem(int i) {
-        int counter = 0;
-        boolean found = false;
-        int result = 0;
-        for (int[] array : values) {
-            for (int value : array) {
-                if (counter == i) {
-                    result = value;
-                    found = true;
-                    break;
-                } else {
-                    counter++;
-                }
-            }
-            if (found) {
-                break;
-            }
-        }
-        return result;
-    }
-
     @Override
     public boolean hasNext() {
-        return index < arrayLength();
+        return row < values.length && columnIndex < values[row].length;
     }
 
     @Override
     public Object next() {
-        if (index >= arrayLength()) {
+        int result = 0;
+        if (row < values.length && columnIndex < values[row].length) {
+            result = values[row][columnIndex];
+            if (columnIndex >= values[row].length - 1) {
+                columnIndex = 0;
+                row++;
+            } else {
+                columnIndex++;
+            }
+        } else {
             throw new NoSuchElementException();
         }
-        return getItem(index++);
+        return result;
     }
 }
