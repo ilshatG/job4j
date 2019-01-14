f5231653c083f663ca981916589bca062c35a9bc
ilshat <ilshatrashidovich@rambler.ru>
2019-01-14 17:56:26 +0500

    1. Произвести автоматическое тестирование всех сервлетов.[#76812]

---
+++ chapter_2_004/src/main/java/servlets/EchoServlets/ValidateStub.java
@@ -0,0 +1,49 @@
+package servlets.EchoServlets;
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ValidateStub implements Store {
+    private final Map<Integer, User> store = new HashMap<>();
+    private int ids = 0;
+
+    @Override
+    public void update(User user) {
+
+    }
+
+    @Override
+    public void delete(User user) {
+
+    }
+
+    @Override
+    public List<User> findAll(User user) {
+        return null;
+    }
+
+    @Override
+    public User findById(User user) {
+        return null;
+    }
+
+    @Override
+    public List<User> getAll() {
+        return new ArrayList<User>(this.store.values());
+    }
+
+    @Override
+    public User currentUser(String login, String password) {
+        return null;
+    }
+
+    @Override
+    public void add(User user) {
+        user.setId(this.ids++);
+        this.store.put(user.getId(), user);
+        return;
+    }
+}
---
+++ chapter_2_004/src/test/java/servlets/EchoServlets/UsersControllerTest.java
@@ -0,0 +1,36 @@
+package servlets.EchoServlets;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(ValidateService.class)
+public class UsersControllerTest {
+    @Test
+    public void addUser() throws ServletException, IOException {
+        Store validate = new ValidateStub();
+        PowerMockito.mockStatic(ValidateService.class);
+        Mockito.when(ValidateService.getInstance()).thenReturn((ValidateService)validate);
+        HttpServletRequest req = mock(HttpServletRequest.class);
+        HttpServletResponse resp = mock(HttpServletResponse.class);
+        when(req.getParameter("name")).thenReturn("Petr Arsentev");
+        new UsersController().doPost(req, resp);
+        assertThat(validate.getAll().iterator().next().getName(), is("Petr Arsentev"));
+    }
+}
